4682b874a8e001f3076e5c0d724060512c7e7d4f
songyuchen2584 <songyuchen2584@gmail.com>
2024-02-12 13:13:43 -0500

    gui version but divide by 0 is giving nan except of throwing an exception

---
+++ Assignment3/Worksheet2/BMI/guiBMI/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace CS_392_HW2_BMU_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblFeet = new System.Windows.Forms.Label();
+            this.lblInches = new System.Windows.Forms.Label();
+            this.lblPounds = new System.Windows.Forms.Label();
+            this.txtFt = new System.Windows.Forms.TextBox();
+            this.txtInches = new System.Windows.Forms.TextBox();
+            this.txtPounds = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblEnglish = new System.Windows.Forms.LinkLabel();
+            this.lblMetric = new System.Windows.Forms.LinkLabel();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblYears = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(40, 345);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(76, 20);
+            this.lblHeight.TabIndex = 0;
+            this.lblHeight.Text = "3. Height";
+            this.lblHeight.Click += new System.EventHandler(this.lblHeight_Click);
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeight.Location = new System.Drawing.Point(40, 483);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(79, 20);
+            this.lblWeight.TabIndex = 1;
+            this.lblWeight.Text = "4. Weight";
+            // 
+            // lblFeet
+            // 
+            this.lblFeet.AutoSize = true;
+            this.lblFeet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFeet.Location = new System.Drawing.Point(87, 391);
+            this.lblFeet.Name = "lblFeet";
+            this.lblFeet.Size = new System.Drawing.Size(42, 20);
+            this.lblFeet.TabIndex = 2;
+            this.lblFeet.Text = "Feet";
+            // 
+            // lblInches
+            // 
+            this.lblInches.AutoSize = true;
+            this.lblInches.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInches.Location = new System.Drawing.Point(203, 390);
+            this.lblInches.Name = "lblInches";
+            this.lblInches.Size = new System.Drawing.Size(58, 20);
+            this.lblInches.TabIndex = 3;
+            this.lblInches.Text = "Inches";
+            // 
+            // lblPounds
+            // 
+            this.lblPounds.AutoSize = true;
+            this.lblPounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPounds.Location = new System.Drawing.Point(84, 522);
+            this.lblPounds.Name = "lblPounds";
+            this.lblPounds.Size = new System.Drawing.Size(65, 20);
+            this.lblPounds.TabIndex = 4;
+            this.lblPounds.Text = "Pounds";
+            // 
+            // txtFt
+            // 
+            this.txtFt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFt.Location = new System.Drawing.Point(90, 420);
+            this.txtFt.Name = "txtFt";
+            this.txtFt.Size = new System.Drawing.Size(100, 27);
+            this.txtFt.TabIndex = 5;
+            this.txtFt.TextChanged += new System.EventHandler(this.txtFt_TextChanged);
+            // 
+            // txtInches
+            // 
+            this.txtInches.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInches.Location = new System.Drawing.Point(206, 419);
+            this.txtInches.Name = "txtInches";
+            this.txtInches.Size = new System.Drawing.Size(100, 27);
+            this.txtInches.TabIndex = 6;
+            // 
+            // txtPounds
+            // 
+            this.txtPounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPounds.Location = new System.Drawing.Point(89, 550);
+            this.txtPounds.Name = "txtPounds";
+            this.txtPounds.Size = new System.Drawing.Size(100, 27);
+            this.txtPounds.TabIndex = 7;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCalc.FlatAppearance.BorderSize = 5;
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCalc.Location = new System.Drawing.Point(89, 595);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(103, 37);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblEnglish
+            // 
+            this.lblEnglish.AutoSize = true;
+            this.lblEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnglish.Location = new System.Drawing.Point(300, 55);
+            this.lblEnglish.Name = "lblEnglish";
+            this.lblEnglish.Size = new System.Drawing.Size(64, 20);
+            this.lblEnglish.TabIndex = 9;
+            this.lblEnglish.TabStop = true;
+            this.lblEnglish.Text = "English";
+            this.lblEnglish.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblEnglish_LinkClicked);
+            // 
+            // lblMetric
+            // 
+            this.lblMetric.AutoSize = true;
+            this.lblMetric.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMetric.Location = new System.Drawing.Point(370, 55);
+            this.lblMetric.Name = "lblMetric";
+            this.lblMetric.Size = new System.Drawing.Size(56, 20);
+            this.lblMetric.TabIndex = 10;
+            this.lblMetric.TabStop = true;
+            this.lblMetric.Text = "Metric";
+            this.lblMetric.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblMetric_LinkClicked);
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut.Location = new System.Drawing.Point(213, 555);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(0, 20);
+            this.lblOut.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(871, 29);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "                    BMI Calculator                                               " +
+    "                                                  \r\n";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(40, 104);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(157, 20);
+            this.lblGender.TabIndex = 13;
+            this.lblGender.Text = "1. Gender (optional)";
+            this.lblGender.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(38, 213);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(56, 20);
+            this.lblAge.TabIndex = 14;
+            this.lblAge.Text = "2. Age";
+            // 
+            // txtGender
+            // 
+            this.txtGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGender.Location = new System.Drawing.Point(86, 149);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(100, 27);
+            this.txtGender.TabIndex = 15;
+            this.txtGender.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(86, 280);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 27);
+            this.txtAge.TabIndex = 16;
+            // 
+            // lblYears
+            // 
+            this.lblYears.AutoSize = true;
+            this.lblYears.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYears.Location = new System.Drawing.Point(82, 247);
+            this.lblYears.Name = "lblYears";
+            this.lblYears.Size = new System.Drawing.Size(52, 20);
+            this.lblYears.TabIndex = 17;
+            this.lblYears.Text = "Years";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 703);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblYears);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.lblMetric);
+            this.Controls.Add(this.lblEnglish);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtPounds);
+            this.Controls.Add(this.txtInches);
+            this.Controls.Add(this.txtFt);
+            this.Controls.Add(this.lblPounds);
+            this.Controls.Add(this.lblInches);
+            this.Controls.Add(this.lblFeet);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblHeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblFeet;
+        private System.Windows.Forms.Label lblInches;
+        private System.Windows.Forms.Label lblPounds;
+        private System.Windows.Forms.TextBox txtFt;
+        private System.Windows.Forms.TextBox txtInches;
+        private System.Windows.Forms.TextBox txtPounds;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.LinkLabel lblEnglish;
+        private System.Windows.Forms.LinkLabel lblMetric;
+        private System.Windows.Forms.Label lblOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblYears;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Assignment3/Worksheet2/BMI/guiBMI/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_392_HW2_BMU_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            calc_catch();
+        }
+
+        private void calc_catch() {
+            try
+            {
+                double bmi = 0;
+                string weight_status = "none";
+                double age = double.Parse(txtAge.Text);
+                string gender = txtGender.Text;
+                string msg = "";
+
+                if (txtAge.Text == "") { 
+                    txtAge.Text = "0";
+                }
+                if (txtFt.Text == "") { 
+                    txtFt.Text = "0";
+                }
+                if (txtInches.Text == "") {
+                    txtInches.Text = "0";
+                }
+                if (txtPounds.Text == "") { 
+                    txtPounds.Text = "0";
+                }
+                if (lblFeet.Text.Equals("Feet"))
+                {
+                    double inches = (double.Parse(txtFt.Text)) * 12 + (double.Parse(txtInches.Text));
+                    double pounds = (double.Parse(txtPounds.Text));
+                    bmi = (pounds / (Math.Pow(inches, 2))) * 703;
+                }
+                else
+                {
+                    double centimeters = (double.Parse(txtFt.Text));
+                    double kilograms = (double.Parse(txtPounds.Text));
+                    bmi = kilograms / (Math.Pow(centimeters / 100, 2));
+                }
+
+                if (bmi < 18.5)
+                {
+                    weight_status = "Underweight";
+                }
+                else if (bmi < 24.9)
+                {
+                    weight_status = "Healthy Weight";
+                }
+                else if (bmi < 29.9)
+                {
+                    weight_status = "Overweight";
+                }
+                else
+                {
+                    weight_status = "Obesity";
+                }
+                if (gender == "")
+                {
+                    msg = ("Your BMI is " + bmi + " at age " + age + ".");
+                }
+                else
+                {
+                    msg = ("You are a " + age + " year old " + gender + " with a BMI of " + bmi + ".");
+                }
+                MessageBox.Show(msg + "\n Weight satus: " + weight_status );
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Check that your values were inputed correctly: " + ex);
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show("Unrealiscly great weight or height: " + ex);
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("Your height cannot be 0: " + ex);
+            }
+            catch (Exception ex) { 
+                MessageBox.Show("Something else is wrong: "+ ex);
+            }
+        }
+
+        private void lblEnglish_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            lblFeet.Text = "Feet";
+            lblPounds.Text = "Pounds";
+            lblInches.Visible = true;
+            txtInches.Visible = true;
+        }
+
+        private void lblMetric_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            lblFeet.Text = "Centimeters";
+            lblPounds.Text = "Kilograms";
+            lblInches.Visible = false;
+            txtInches.Visible = false;
+        }
+
+        private void lblHeight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
